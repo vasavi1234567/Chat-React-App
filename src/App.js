a4054f18da53b9fd316bb8e82f26df490d566934
vasavi1234567 <vasaviyepalagunta5@gmail.com>
2024-12-01 20:37:42 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import MainContainer from './components/MainContainer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-container">
+        <img src="/logos/Frame 1171287361.png" alt="frame" />
+      </div>
+      <div className="right-container">
+          <MainContainer />
+      </div>
     </div>
   );
 }
---
+++ src/components/MainContainer/index.js
@@ -0,0 +1,43 @@
+import './index.css'
+
+const MainContainer = () => {
+    return (
+      <div>
+        <div className="header-container">
+            <img src="/logos/Group 1171287132.png" alt="group" />
+        </div>
+        <div className="home-container">
+            <div className="top-container">
+                <img src="/logos/Frame 1171287402.png" alt="frame-7402" /> 
+            </div>
+            <div className="below-container">
+                <div>
+                    <img src="/logos/Frame 1171287302.png" alt="frame-7302" />
+                </div>
+                <div className="middle-container">
+                    <div className="middle-header-container">
+                        <img src="/logos/Frame 1171287411.png" alt="frame-7411" />
+                        <img src="/logos/Frame 1171287287.png" alt="frame-7287" />
+                    </div>
+                    <div>
+                        <img src="/logos/Frame 1171287404.png" alt="frame-7404" />
+                    </div>
+                </div>
+                <div className="below-right-container">
+                    <div>
+                        <img src="/logos/Frame 1171287414.png" alt="frame-7414" />
+                    </div>
+                    <div>
+                        <img src="/logos/Frame 1171287412.png" alt="frame-7412" />
+                    </div>
+                    <div>
+                        <img src="/logos/Frame 1171287413.png" alt="frame-7413" />
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+export default MainContainer
